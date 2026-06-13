d90a5f5ec717d7289a3d7a1ea8b58a0a5db7c83c
Hamza <hamzajaved6867983@gmail.com>
2026-06-13 19:32:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ def create_app(config_class=Config):
 
     return app
 
-
-if __name__ == "__main__":
+ app = create_app()
     app = create_app()
     app.run(debug=True, host="0.0.0.0", port=5000)
